feaf57a0e7032714a6f2066ea79cd323bd1416e3
theunhackable <srirangasaipagala@gmail.com>
2023-09-23 14:42:36 +0530

    added production url

--- src/core/utils/index.js
+++ src/core/utils/index.js
@@ -1,3 +1,3 @@
 const production =  process.env.NODE_ENV === 'production'
 
-export const SITE_URL = production ? 'prod-url': 'http://localhost:3000'
+export const SITE_URL = production ? 'https://epic-sass-ruddy.vercel.app/': 'http://localhost:3000'
